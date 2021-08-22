83ff026dc987c0b024e52af19eba48943d157212
Wilhelm Liao <williaml.twcp@gmail.com>
2021-08-22 17:20:33 +0800

    pre alpha

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/bcowtech/go-tools
 
 go 1.14
+
+require (
+	golang.org/x/mod v0.4.2
+	golang.org/x/tools v0.1.0
+)
---
+++ gorun/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+var (
+	pos int = 0
+)
+
+func main() {
+	var (
+		args = []string{"godotenv"}
+	)
+
+	// parse godotenv flag
+	flag := shift()
+	switch flag {
+	case "-f":
+		args = append(args, flag, shift())
+		args = append(args, "go", "run")
+		args = append(args, arguments()...)
+	case ".":
+		// skip
+	default:
+		args = append(args, "go", "run", flag)
+		args = append(args, arguments()...)
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		throw(err.Error())
+		os.Exit(1)
+	}
+}
+
+func throw(err string) {
+	fmt.Fprintln(os.Stderr, err)
+}
+
+func shift() string {
+	if pos < len(os.Args) {
+		pos++
+		return os.Args[pos]
+	}
+	return ""
+}
+
+func arguments() []string {
+	if pos < len(os.Args) {
+		return os.Args[pos+1:]
+	}
+	return nil
+}
